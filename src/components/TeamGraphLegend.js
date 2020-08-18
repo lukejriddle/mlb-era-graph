88cdb1565e74c9f1390108e06b4ef8c93662708f
Luke Riddle <luke@Lukes-MacBook-Pro.local>
2020-08-18 17:02:51 -0400

    Change legend

--- src/components/TeamGraphLegend.js
+++ src/components/TeamGraphLegend.js
@@ -5,30 +5,28 @@ function TeamGraphLegend() {
         <div className="d-flex flex-column align-items-center mt-4">
             <svg style={{width:'80%', height:'30px', overflow:'visible'}}>
                 <rect style={{fill:'rgb(0,100,0)', height:"14px", width:'14px'}}/>
-                <text fontFamily="Courier New" alignmentBaseline="top" x={16} y={12} style={{fill:'black', height:"20%", width:'20%'}}>Starter</text>
+                <text fontFamily="Courier New" alignmentBaseline="top" x={18} y={12} style={{fill:'black', height:"20%", width:'20%'}}>Starter</text>
             </svg>
 
             <svg style={{width:'80%', height:'30px', overflow:'visible'}}>
                 <rect style={{fill:'rgb(0,99,166)', height:"14px", width:'14px'}}/>
-                <text fontFamily="Courier New" alignmentBaseline="top" x={16} y={12} style={{fill:'black', height:"20%", width:'20%'}}>Reliever</text>
+                <text fontFamily="Courier New" alignmentBaseline="top" x={18} y={12} style={{fill:'black', height:"20%", width:'20%'}}>Reliever</text>
             </svg>
 
-            <svg style={{width:'80%', height:'30px', overflow:'visible'}}>
+            <svg style={{width:'80%', height:'90px', overflow:'visible'}}>
                 <defs>
-                    <linearGradient spreadMethod="pad" id="gradientStarter" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <linearGradient spreadMethod="pad" id="gradientStarter" x1="0%" y1="100%" x2="0%" y2="0%">
                         <stop offset="0%" style={{'stop-color':'rgba(0, 100, 0, .1)', 'stop-opacity':1}} />
-                        <stop offset="50%" style={{'stop-color':'rgba(0, 100, 0, .4)', 'stop-opacity':1}} />
+                        
                         <stop offset="100%" style={{'stop-color':'rgb(0, 100, 0)', 'stop-opacity':1}} />
                     </linearGradient>
-                    <linearGradient spreadMethod="pad" id="gradientReliever" x1="0%" y1="0%" x2="100%" y2="0%">
-                        <stop offset="0%" style={{'stop-color':'rgba(0, 99, 166, .1)', 'stop-opacity':1}} />
-                        <stop offset="50%" style={{'stop-color':'rgba(0, 99, 166, .4)', 'stop-opacity':1}} />
-                        <stop offset="100%" style={{'stop-color':'rgb(0, 99, 166)', 'stop-opacity':1}} />
-                    </linearGradient>
                 </defs>
-                <rect width="100%" height="14px" y="0" x="0" fill="url(#gradientStarter)"/> 
-                <rect width="100%" height="14px" y="16" x="0" fill="url(#gradientReliever)"/> 
-                <text fontFamily="Courier New" y={50}># Innings</text>
+                <rect width="14px" height="100%" y="0" x="0" fill="url(#gradientStarter)"/> 
+                <text fontSize="14px" fontFamily="Courier New" y={12} x={18}>More innings</text>
+                <text fontSize="14px" fontFamily="Courier New" y={26} x={18}>pitched</text>
+
+                <text fontSize="14px" fontFamily="Courier New" y={76} x={18}>Fewer innings</text>
+                <text fontSize="14px" fontFamily="Courier New" y={90} x={18}>pitched</text>
             </svg>
         </div>
     )
